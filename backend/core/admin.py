fb597f0c0fb3fd6ca70d0173d27225c32850e44e
Ilyas B <ec221241@qmul.ac.uk>
2026-03-05 13:20:57 +0000

    Register core models in Django admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Holding, ForecastRun, AuditEvent, KnowledgeSnippet
 
-# Register your models here.
+admin.site.register(Holding)
+admin.site.register(ForecastRun)
+admin.site.register(AuditEvent)
+admin.site.register(KnowledgeSnippet)
